05a6568fe4a7ecf6f946aa8db4cb8964500b9cc7
cupertank <ilyavologin@gmail.com>
2020-05-24 15:14:00 +0300

    eat -> eta

--- hw3/task3.1.hs
+++ hw3/task3.1.hs
@@ -1,7 +1,7 @@
 -- func x l = map (\y -> y * x) l        #
--- func x = map (\y -> y * x)            # eat редукция l
+-- func x = map (\y -> y * x)            # eta редукция l
 -- func x = map (*x)			 # Замена лямбды на частичную функцию
 -- func x = (map . (*)) x		 # Композиция между map и (*), вынос x
--- func = map . (*)			 # eat редукция x
+-- func = map . (*)			 # eta редукция x
 
 func = map . (*)
