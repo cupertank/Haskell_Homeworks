e71938fe4026799325fbe07b39d25230d25956c2
cupertank <ilyavologin@gmail.com>
2020-06-06 12:06:07 +0300

    Finally fix task3.4

--- hw3/task3.4.hs
+++ hw3/task3.4.hs
@@ -1,13 +1,16 @@
 bracketChecker str = bracketHelper str []
 
-bracketHelper :: String -> [Char] -> Bool
+bracketHelper :: String -> [Int] -> Bool
 bracketHelper (char:other) stack
-    | char == '(' ||
-      char == '[' ||
-      char == '{' = bracketHelper other (char:stack)
-    | otherwise = 
-        if length stack /= 0 && (char == ')' || char == ']' || char == '}') && head stack == char then
-            bracketHelper other (tail stack)
+    | char == '(' = bracketHelper other (1:stack)
+    | char == '[' = bracketHelper other (2:stack)
+    | char == '{' = bracketHelper other (3:stack)
+    | otherwise   = 
+        if length stack /= 0 && (
+            char == ')' && head stack == 1 ||
+            char == ']' && head stack == 2 ||
+            char == '}' && head stack == 3
+            ) then bracketHelper other (tail stack)
         else
             False
 
